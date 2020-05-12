d3e4a52739c4f164f06d25f1b3376d300670475a
trungld3 <k21tpm4@gmail.com>
2020-05-12 12:20:21 +0700

    finish

--- AR ruler/ViewController.swift
+++ AR ruler/ViewController.swift
@@ -12,6 +12,7 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -24,6 +25,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
       
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
         print("touched")
         if let touchLocation = touches.first?.location(in: sceneView)
         {
@@ -64,8 +71,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let distance = sqrt(pow(end.position.x - start.position.x, 2) + pow(end.position.y - start.position.y, 2) + pow(end.position.z - start.position.z, 2))
         
-        print(abs(distance))
+      //  print(abs(distance))
+        updateText(text: "\(abs(distance))", atPosition : end.position)
 
+    }
+    func updateText ( text : String, atPosition position : SCNVector3) {
+        
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText( string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        textNode = SCNNode(geometry: textGeometry)
+        textNode.position =  SCNVector3( position.x    , position.y  + 0.01  , position.z)
+        textNode.scale = SCNVector3( 0.01 , 0.01, 0.01)
+        sceneView.scene.rootNode.addChildNode(textNode)
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
